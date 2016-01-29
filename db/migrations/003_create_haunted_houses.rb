20138789d77b391df50c128d88a0cd0ef0dfac6b
zacscodingclub <zbaston@gmail.com>
2016-01-28 21:45:49 -0600

    costume and costume store specs passing.  working on haunted house

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :friendly
+      t.dateimte :opening_time
+      t.datetime :closing_time
+      t.text :description
+    end
+  end
+end
