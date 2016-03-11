fa09781dbe772b9c62d1222167afed87ccce0c2f
lawrend <douglaslawrence314@gmail.com>
2016-03-11 15:47:05 -0800

    done

--- db/migrations/002_create_costume_stores.rb
+++ db/migrations/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration 
+  def change
+    create_table :costume_stores do |i|
+      i.text :name
+      i.text :location
+      i.integer :costume_inventory
+      i.integer :num_of_employees
+      i.boolean :still_in_business
+      i.datetime :opening_time
+      i.datetime :closing_time
+    end
+  end
+end
--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses <ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |i|
+      i.text :name
+      i.text :location
+      i.text :theme
+      i.integer :price
+      i.boolean :family_friendly
+      i.datetime :opening_date
+      i.datetime :closing_date
+      i.text :description
+    end
+  end
+end
