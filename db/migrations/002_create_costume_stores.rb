8334de227929f7cbcea63366186a7a6762b2bcc7
ShiLi007 <lishidaniel@gmail.com>
2016-03-25 11:58:32 -0400

    Done.

--- db/migrations/002_create_costume_stores.rb
+++ db/migrations/002_create_costume_stores.rb
@@ -10,7 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
-      t.string :closing_time
+      t.datetime :closing_time
       
       t.timestamps null:false
     end
