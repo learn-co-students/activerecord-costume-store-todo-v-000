9eb422bc3b4fa0189e6e4f34123a475e8915a418
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-29 15:01:52 -0700

    Done.

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,20 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change 
+    create_table :haunted_houses do |t|
+      t.text :name
+      t.text :theme
+      t.text :location
+      t.text :description
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      # t.integer :costume_inventory
+      # t.integer :num_of_employees
+      # t.boolean :still_in_business
+      # t.datetime :opening_time
+      # t.datetime :closing_time
+    end
+  end
+end
