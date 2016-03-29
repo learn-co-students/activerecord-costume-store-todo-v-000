055b3ec93f7e600a063e16acfb74e351c51fbd20
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-29 15:02:48 -0700

    Done.

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -10,11 +10,6 @@ class CreateHauntedHouses < ActiveRecord::Migration
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      # t.integer :costume_inventory
-      # t.integer :num_of_employees
-      # t.boolean :still_in_business
-      # t.datetime :opening_time
-      # t.datetime :closing_time
     end
   end
 end
