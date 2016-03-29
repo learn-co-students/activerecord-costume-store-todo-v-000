5470ebde4eb427aa170dce3189c1a7c7c51835bf
bedwardsphoto <brian@brianedwardsphotography.com>
2016-03-29 11:03:07 -0400

    Done.

--- db/migrations/001_create_costumes.rb
+++ db/migrations/001_create_costumes.rb
@@ -7,3 +7,16 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+class CreateCostumes<ActiveRecord::Migration
+
+  def change
+    create_table :costumes do |a|
+      a.string :name
+      a.integer :price
+      a.string :size
+      a.string :image_url
+      a.timestamps
+    end
+  end
+
+end
--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses<ActiveRecord::Migration
+
+    def change
+      create_table :haunted_houses do |a|
+        a.string :name
+        a.string :location
+        a.string :theme
+        a.integer :price
+        a.boolean :family_friendly
+        a.datetime :opening_date
+        a.datetime :closing_date
+        a.string :description
+      end
+    end
+
+end
