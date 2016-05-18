17d2fd3df53ab0fa9a1b8731ab46ef7094a9560c
karuna24s <karuna24s@gmail.com>
2016-05-17 23:47:23 -0400

    Done.

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -10,7 +10,6 @@ class CreateHauntedHouses < ActiveRecord::Migration
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-      #t.timestamps
     end
   end
 end
