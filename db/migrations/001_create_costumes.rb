6930e088c0e0ac495e05b71ffcc5425c8eb6f879
rnhutsko <rnhutsko@gmail.com>
2016-06-20 12:36:04 -0400

    created code for migrations of costumes, stores, and haunted houses

--- db/migrations/001_create_costumes.rb
+++ db/migrations/001_create_costumes.rb
@@ -1,3 +1,4 @@
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
@@ -7,3 +8,15 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+create Costume < ActiveRecord::Migration
+  def change
+       create_table :costumes do |t|
+        t.string :name
+        t.float :price
+        t.string :size
+        t.string :image_url
+      end
+  end
+
+end
--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+create HauntedHouse < ActiveRecord::Migration
+  def change
+       create_table :hauntedhouses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :price
+        t.boolean :family_friendly
+        t.datetime :opening_date
+        t.datetime :closing_date
+        t.string :description
+      end
+  end
+
+end
