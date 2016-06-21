fe5d030ff75eb563d0d47d46e2137957c0d7aedf
kaileeagray <kaileeagray@gmail.com>
2016-06-20 20:33:10 -0500

    Done.

--- db/migrations/001_create_costumes.rb
+++ db/migrations/001_create_costumes.rb
@@ -5,5 +5,25 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+# # The costumes table will have four columns:
+# name
+# price
+# size
+# # image url
+# stick to some specific naming conventions: while the migrations are plural, the models are singular.
+
+class CreateCostumes < ActiveRecord::Migration
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.decimal :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+
+end
--- db/migrations/002_create_costume_stores.rb
+++ db/migrations/002_create_costume_stores.rb
@@ -1,1 +1,25 @@
-# Create your costume_stores migration here
+# # Create your costume_stores migration here
+# The costume_stores table will have seven columns:
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
+class CreateCostumeStores < ActiveRecord::Migration
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.timestamps
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+
+end
