fb50c64083d8a95071f31f92f9ab9428cbfd9b52
Joseph Lim <joseph.d.lim@gmail.com>
2016-06-24 20:05:27 +0000

    Done.

--- db/migrations/002_create_costume_stores.rb
+++ db/migrations/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+  def update
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :still_open
+      t.datetime :openeing_time
+      t.datetime :closing_time
+    end
+  end
+end
