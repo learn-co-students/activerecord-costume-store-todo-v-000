bdd2ed8d33138240aa4e727d79bf44ff033228c3
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-10 04:16:56 +0000

    Done.

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.date :closing_date
+    end
+  end
+end
