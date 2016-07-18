f10131989a5f2f85e1da3f6fe5a81ededd5aa4ce
Adam Taitano <adam.taitano@gmail.com>
2016-07-18 18:16:28 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,7 +16,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps null: false
     end
   end
 
