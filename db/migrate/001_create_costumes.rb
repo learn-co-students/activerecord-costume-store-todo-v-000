1685b31005156e798d4f1e76ba497efae825959e
auranbuckles <auran59@gmail.com>
2016-07-31 18:57:33 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,10 +13,11 @@ class CreateCostumes < ActiveRecord::Migration
 	def change
 		create_table :costumes do |t|
 			t.string :name
-			t.integer :price
+			t.float :price
 			t.string :image_url
 			t.string :size
-			t.timestamps
+			
+			t.timestamps null: false
 		end
 	end
 
