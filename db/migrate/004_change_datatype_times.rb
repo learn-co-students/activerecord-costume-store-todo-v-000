12950a35fdf9ed042ba662528837fd311c1c9efc
William Jones <enquiries@forestfed.com>
2016-08-25 20:20:17 +0000

    use AR to create DBs

---
+++ db/migrate/004_change_datatype_times.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeTimes < ActiveRecord::Migration
+
+  def change
+    change_column(:costume_stores, :opening_time, :datetime)
+    change_column(:costume_stores, :closing_time, :datetime)
+  end
+end
