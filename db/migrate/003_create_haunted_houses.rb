734a248050b3951308842d6fa490a396f32c4e59
Emmanuel Owusu <e_owusu@live.com>
2016-10-20 22:46:47 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.boolean :family_friendly
+      h.timestamp :opening_date
+      h.timestamp :closing_time
+      h.text :description
+    end
+  end
+end
