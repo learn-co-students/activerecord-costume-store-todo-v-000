dedaefaa27fb32a96730b4afbd2e88b1e1a5ee05
Laurent Milne <lmilne360@gmail.com>
2016-11-20 21:58:20 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,12 @@
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[5.0]
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps 
+      t.timestamps
     end
   end
 end
