0355feb4f787f500a7fcb3f024c03f73f2c8cfe2
Senad Hadrovic <senadhadrovic@gmail.com>
2016-12-24 05:12:08 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,12 +3,14 @@
 class CreateCostumeStores < ActiveRecord::Migration
 
   def change
-    create_table :stores do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
-      t.integer :employee_count
-      t.boolean :open 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+    end
+  end
+end
