80e667d7387584fb575a40e59b8e1de75d7ca977
Tayari Appiah <cjhhh.ta@gmail.com>
2016-12-31 17:28:39 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration
   def change
    create_table :costumes do |c|
     c.string :name
-    c.integer  :price
+    c.float :price
     c.string :image_url
     c.string :size
 
