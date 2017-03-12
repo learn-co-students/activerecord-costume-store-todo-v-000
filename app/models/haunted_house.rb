59e83dea1fca439fd0ffe5f299ab5bc0d1ee0cbe
cquinones100 <cquinones100@gmail.com>
2017-03-12 19:42:04 -0400

    Done.

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,14 @@
 # Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Base
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS haunted_houses (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+      SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+end
