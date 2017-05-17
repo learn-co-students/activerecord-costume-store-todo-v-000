3bac158cb758a146357afdd0d86a35f09e261ac5
Villy Siu <villysiu@gmail.com>
2017-05-17 21:54:45 +0000

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,19 +1,4 @@
 # Create your CostumeStore class here
 class CostumeStore < ActiveRecord::Base
-  def self.create_table
-    # create table using SQL here
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS costume_stores (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      costume_inventory INTEGER,
-      num_of_employees INTEGER,
-      still_in_business TEXT,
-      opening_time TEXT,
-      closing_time TEXT
-    )
-    SQL
 
-    ActiveRecord::Base.connection.execute(sql)
-  end
 end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,21 +1,4 @@
 # Create your HauntedHouse class here
 class HauntedHouse < ActiveRecord::Base
-  def self.create_table
-    # create table using SQL here
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS haunted_houses (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      loaction TEXT,
-      theme TEXT,
-      price REAL,
-      family_friendly TEXT,
-      opening_date TEXT,
-      closing_date TEXT
-      description TEXT
-    )
-    SQL
-
-    ActiveRecord::Base.connection.execute(sql)
-  end
+  
 end
