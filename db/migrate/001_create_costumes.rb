edeb748c6dc5811d74cbcdadb009fe382ab2e0a8
Catherine Cordova <ccordova41@gmail.com>
2017-07-16 01:03:57 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,11 @@
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.string :size
       t.string :image_url
       t.float :price
-
+      t.timestamps null: false
     end
   end
 end
