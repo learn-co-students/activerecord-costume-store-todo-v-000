8961dbb1be11cf883c4ea9b8eb3e241b4de0d2c2
Tyler Gotto <tylergotto17@gmail.com>
2017-07-18 03:01:47 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
-# Create your haunted_houses migration here
+# Create your haunted_houses migration hereclass CreateCostumes < ActiveRecord::Migration[5.0]
+class CreateHauntedHouses < ActiveRecord::Migration[5.0]
+
+	def change
+	    create_table :haunted_houses do |t|
+	      t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :price
+        t.boolean :family_friendly
+        t.string :opening_date
+        t.string :closing_date
+        t.text :description
+	    end
+  	end
+
+end
