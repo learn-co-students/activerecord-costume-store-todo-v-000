29433f23c049be224aceaaafb689e3186ec35415
mikka <mikkanthrope@gmail.com>
2017-07-24 23:50:11 -0700

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,20 @@
 # Create your haunted_houses migration here
+
+# Create your CostumeStore class here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do |house|
+      house.string :name
+      house.string :location
+      house.string :theme
+      house.integer :price
+      house.boolean :family_friendly
+      house.date :opening_date
+      house.date :closing_date
+      house.text :description
+    end
+  end
+
+end
