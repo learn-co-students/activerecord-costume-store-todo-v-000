54ecab698f444169377b5e49b39ffa9d3f08cbf4
jonathan <jonathanyi81@gmail.com>
2017-08-16 22:57:53 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.time :opening_time
-      t.time :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamp
     end
   end
 end
