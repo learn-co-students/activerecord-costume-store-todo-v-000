cd424fcbd88856338d9bf8f66b6a63d178c7710f
lpassamano <leighayna.josele@gmail.com>
2017-09-06 19:12:08 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |column|
+      column.string :name
+      column.string :location
+      column.integer  :costume_inventory
+      column.integer :num_of_employees
+      column.boolean :still_in_business
+      column.timestamp :opening_time
+      column.timestamp :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |column|
+      column.string :name
+      column.string :location
+      column.string :theme
+      column.float :price
+      column.boolean :family_friendly
+      column.datetime :opening_date
+      column.datetime :closing_date
+      column.text :description
+    end
+  end
+end
