9654c142901aaf3856830e16963950ea1cb9bbbb
Elvin <Dimix181@Gmail.com>
2017-09-09 00:51:11 -0400

    model test are passing

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+
+  def change
+      create_table :hauntedhouses do |t|
+        t.string   :name
+        t.string   :location
+        t.integer  :costume_inventory
+        t.integer  :number_of_employees
+        t.string   :still_in_business
+        t.integer  :opening_time
+        t.integer  :closing_time
+      end
+  end
+
+
+end
 # Create your haunted_houses migration here
