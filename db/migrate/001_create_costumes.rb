0282716cc4477909b62ffa8b8fe667d41654c155
github username <github email address>
2017-09-21 00:44:30 -0700

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,10 +11,11 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :image_url
       t.string :size
-      t.timestamps
+
+      t.timestamps null:false
     end
   end
 end
