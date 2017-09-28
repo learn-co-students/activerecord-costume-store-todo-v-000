a03e86bf1a504fe3f83b7f2e1c385527e0b316bc
Alexander Friedland <friedlandaj@gmail.com>
2017-09-28 02:46:41 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,5 +5,18 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |x|
+      x.string :name
+      x.float :price
+      x.string :image_url
+      x.string :size
+      #x.datetime :created_at
+      x.timestamps
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |x|
+      x.string :name
+      x.string :location
+      x.string :theme
+      x.integer :price
+      x.boolean :family_friendly
+      x.datetime :opening_date
+      x.datetime :closing_date
+      x.string :description
+    end
+  end
+end
