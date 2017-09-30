186e90637efd050af0d68c6b93b85901b8212ba9
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-29 17:01:52 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.text "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -19,6 +29,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "size"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.string "description"
   end
 
 end
