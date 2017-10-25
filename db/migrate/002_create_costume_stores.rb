9446dd7acabbe2ea92cf1c34fcc153b029309175
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-25 01:32:27 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |costume_store|
+      costume_store.string :name
+      costume_store.string :location
+      costume_store.boolean :still_in_business, default: true
+      costume_store.datetime :opening_time
+      costume_store.datetime :closing_time
+
+      costume_store.integer :costume_inventory
+      costume_store.integer :num_of_employees
+
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |haunted_house|
+      haunted_house.string :name
+      haunted_house.string :location
+      haunted_house.string :theme
+      haunted_house.numeric :price
+      haunted_house.datetime :opening_date
+      haunted_house.datetime :closing_date
+
+      haunted_house.boolean :family_friendly
+      haunted_house.string :description
+
+
+    end
+  end
+end
