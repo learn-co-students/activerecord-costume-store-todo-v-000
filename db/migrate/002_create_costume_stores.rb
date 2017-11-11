4a791aed538f15959ec781c7341224506e1f1443
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-11-10 19:18:55 -0500

    all tests passing

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,14 +2,16 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
-  create_table :costume_stores do |t|
-    t.text :name
-    t.text :location
-    t.integer :costume_inventory
-    t.integer :num_of_employees
-    t.boolean :still_in_business
-    t.datetime :opening_time
-    t.datetime :closing_time
+  def change
+    create_table :costume_stores do |t|
+      t.text :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
   end
-
+  
 end
