7f26769d843b4d1a0dbf7829b70f7485860d38d1
Adam Fudge <coolfudge@gmail.com>
2017-11-13 21:17:06 +0000

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,13 +1,2 @@
 class CostumeStore < ActiveRecord::Base
-  def change
-    create_table :costume_stores do |t|
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.string :opening_time
-      t.string :closing_time
-    end
-  end
 end
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,13 +8,15 @@
 # In this change method, create columns with the correct names and
 # value types according to the spec
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.integer :size
       t.string :image_url
+
+      t.timestamps
     end
   end
 end
