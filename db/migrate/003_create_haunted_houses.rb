db1dae5adfeaacabb37c6458a9a62acc76d9f79e
Naomi Dennis <njdennis94@gmail.com>
2017-11-16 19:13:10 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :haunted_houses do |col|
+      col.string :name
+      col.string :location
+      col.integer :price
+      col.string :theme
+      col.boolean :family_friendly
+      col.datetime :closing_date
+      col.datetime :opening_date
+      col.text :description
+    end
+  end
+end
