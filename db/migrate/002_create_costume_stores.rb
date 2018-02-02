a4351fdbc520ddbdf5ddbaeaaaa7d4bd20aff4ac
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 20:39:27 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |el|
+      el.string :name
+      el.string :location
+      el.integer :costume_inventory
+      el.integer :num_of_employees
+      el.boolean :still_in_business
+      el.datetime :opening_time
+      el.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |el|
+      el.text :name
+      el.text :location
+      el.text :theme
+      el.float :price
+      el.boolean :family_friendly
+      el.datetime :opening_date
+      el.datetime :closing_date
+      el.text :description
+    end
+  end
+end
