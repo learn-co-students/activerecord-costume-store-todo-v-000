c64a0189c6a34a25caaad41bf92659d28845eeb6
Obafemi Fadairo <ofpupp@princeton.edu>
2018-02-25 04:04:32 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |house|
+      house.string :name
+      house.text :location
+      house.text :theme
+      house.integer :price
+      house.boolean :family_friendly
+      house.text :opening_date
+      house.text :closing_date
+      house.text :description
+    end
+  end
+end
