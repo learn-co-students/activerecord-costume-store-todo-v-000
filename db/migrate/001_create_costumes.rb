22755f6730f3fd0f0d11eecf230419a90cf79e39
Eden Mesfun <emesfun@gmail.com>
2018-03-16 16:32:42 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,16 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumes do |type|
+    type.string :name
+    type.float :price
+    type.string :size
+    type.string :url
+    end #do iteration
+  end
+
+end
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
@@ -5,5 +18,5 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumestores do |type|
+    type.string :name
+    type.float :price
+    type.string :size
+    type.string :url
+    type.datetime :opening_time
+    type.datetime :closing_time
+    end #do iteration
+  end #change method
+
+end
