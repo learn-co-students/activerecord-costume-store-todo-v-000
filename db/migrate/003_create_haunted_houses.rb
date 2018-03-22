6278254b3f5e931c04b809df32c3688050479593
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-22 11:10:16 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |c|
+      c.string :name
+      c.string :location
+      c.string :theme
+      c.integer :price
+      c.boolean :family_friendly
+      c.datetime :opening_date
+      c.datetime :closing_date
+      c.text :description
+    end
+  end
+end
+
 # Create your haunted_houses migration here
