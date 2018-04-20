24678c9ddb33f4a75773c180f95d7235d2bab86f
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-20 22:11:53 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :image_url
+      t.text :image_url
       t.string :size
 
       t.timestamp null:false
