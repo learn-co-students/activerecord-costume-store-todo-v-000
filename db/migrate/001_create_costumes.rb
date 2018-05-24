1cc2f4a309cc6b3118b99c65ea38a09a0ab162da
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-24 00:47:07 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
     t.string :name
     t.integer :price
-    t.string :url
+    t.string :image_url
     t.string :size
+    t.timestamps
     end
   end
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.integer :inventory
+    t.string :description
+    t.float :price
+    t.string :theme
+    t.string :closing_date
+    t.string :opening_date
+    t.boolean :family_friendly
+    end
+  end
+end
