d5030e1d0c08ca0b4ef481acb46aec47d4e457d6
Christine Cua <cloudycua@gmail.com>
2018-06-01 03:19:23 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,6 +11,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.string :description
+
+      t.timestamps
     end
   end
 end
