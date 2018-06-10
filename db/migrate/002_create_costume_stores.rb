810c6bd10e8e441152ee601684438cff843b9404
Christiana Vandermale <christianavdm@gmail.com>
2018-06-10 21:24:53 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,10 +4,10 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employee_count
-      t.string :closed
-      t.integer :opening_time
-      t.integer :closing_time
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
