f552ba0a103c7bd7a44b165818276f664ed50f38
Christiana Vandermale <christianavdm@gmail.com>
2018-06-10 21:19:09 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change 
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employee_count
+      t.string :closed
+      t.integer :opening_time
+      t.integer :closing_time
+    end
   end
 end
