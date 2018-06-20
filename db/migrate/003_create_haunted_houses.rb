e3e73e7964d5c3cec53e1882e3f30ff0a8665975
Anu Khambete <anukhambete@gmail.com>
2018-06-20 21:24:17 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,10 +6,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.integer :price
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-      t.boolean :family_friendly
 
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.integer "price"
+    t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.text "description"
