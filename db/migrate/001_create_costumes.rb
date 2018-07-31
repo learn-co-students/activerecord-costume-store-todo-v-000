62511b92a486f485a196a0163ad4fb2e5eb2bb5e
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-07-31 10:59:32 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,10 @@
 class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
-      t.string :name
+      t.string :name,
+      t.integer :price,
+      t.string :image_url
+      t.string :size,
+      t.timestamps
     end
 end
