321c21901b7ce7c56891b022a949b664e0d50dfa
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-02 14:24:42 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.float :price 
+      t.string :location
+      t.string :theme
+      t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date
+      t.string :description
+    end
+  end
+end
