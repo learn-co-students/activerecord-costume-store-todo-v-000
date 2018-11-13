33eea56a8c0c42e8869eb37009ced8f446157964
Harley Rayner <harleymteden@gmail.com>
2018-11-13 18:46:50 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,18 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores do |costume_store|
+      costume_store.string :name
+      costume_store.text :location
+      costume_store.text :costume_inventory
+      costume_store.integer :num_of_employees
+      costume_store.boolean :still_in_business
+      costume_store.datetime :opening_time
+      costume_store.datetime :closing_time
+      costume_store.timestamps
+    end
+  end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :haunted_houses do |haunted_house|
+      haunted_house.string :name
+      haunted_house.text :location
+      haunted_house.text :theme
+      haunted_house.integer :price
+      haunted_house.boolean :family_friendly
+      haunted_house.datetime :opening_date
+      haunted_house.datetime :closing_date
+      haunted_house.text :description
+      haunted_house.timestamps
+    end
+  end
+
+end
