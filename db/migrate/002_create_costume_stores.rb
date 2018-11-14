02def6eb8f533913a384e7f8563b4e8bbc5a0352
Heidi Hlavinka <hchlav@gmail.com>
2018-11-14 02:24:40 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,21 +1,15 @@
 # Create your costume_stores migration here
-class CostumeStore < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes do |t|
+    create_table :costume_stores do |t|
       t.string :name
-      t.float :price
-      t.string :size
-      t.string :image_url
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
     end
   end
 end
-
-
-# name
-# location
-# number of costumes, or "costume inventory"
-# number of employees
-# whether or not it's still in business
-# opening time
-# closing time
