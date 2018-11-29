f9dc85473bea0f865d74dd3c3b65f480cc448cd3
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-29 19:50:44 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,21 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string  :name
+      t.string  :location
+      t.string  :theme
+      t.decimal  :price, :precision => 10, :scale => 2
+      t.boolean  :family_friendly
+      t.time  :opening_date
+      t.time  :closing_date
+      t.text :description
+      t.timestamps
+    end 
+  end
+  
+  def validate 
+    errors.add(:price, "should be at least 0.01") if price.nil? || price < 0.01 
+  end 
+  
+end
