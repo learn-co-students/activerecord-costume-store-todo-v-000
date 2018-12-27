33eac479332ae72318fd529be8a165379c82ce21
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-27 14:42:04 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :number_of_costumes 
       t.integer :number_of_employees 
-      t.boolean :in_business
+      t.boolean :still_in_business
       t.datetime :opening_time 
       t.datetime :closing_time
       t.datetime :created_at 
