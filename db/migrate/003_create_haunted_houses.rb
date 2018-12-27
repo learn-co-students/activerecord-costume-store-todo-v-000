6799d0c75dfd32b6a8585325f1cfb66992dd13b8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-27 14:33:58 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+     create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price 
+      t.string :family_friendly? 
+      t.string :opening_date 
+      t.string :closing_date 
+      t.text :long_description
+      t.datetime :created_at 
+      t.datetime :updated_at
+     end
+  end
+end 
