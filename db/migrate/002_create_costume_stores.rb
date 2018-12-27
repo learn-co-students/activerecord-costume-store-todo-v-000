b05a4c58ee846f2328be087567a088836bd7c414
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-27 14:29:09 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
-# Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+     create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size 
+      t.text :image_url
+      t.datetime :created_at 
+      t.datetime :updated_at
+     end
+  end
+end 
