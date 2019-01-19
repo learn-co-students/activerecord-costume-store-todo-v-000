cc9c721fdb0de65097512a5fe0eff48d1b4b48a9
Stef Claus <stef.claus@gmail.com>
2019-01-19 19:07:29 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.string :start_time
-      t.string :end_time
+      t.string :closing_time
+      t.string :opening_time
     end #ends iteration
   end #ends method
 end #ends class
