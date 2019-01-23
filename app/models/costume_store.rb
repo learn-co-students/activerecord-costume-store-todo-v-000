3762936255536218fdf7c7c19aa297c3e40f8f63
Cheryl K <cherylkelson@icloud.com>
2019-01-23 23:28:55 +0000

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,14 @@
 # Create your CostumeStore class here
+# class CostumeStore < ActiveRecord::Base
+#   columns:
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
+#
+#
+#
+# end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,13 @@
 # Create your HauntedHouse class here
+# class HauntedHouse < ActiveRecord::Base
+#   columns:
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
+#
+# end
