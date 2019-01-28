9897ac920f59fd48a76eab0766594f7be72967aa
Ryan Brady <ryanbrady369@gmail.com>
2019-01-28 23:54:20 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |q|
+      q.string :name
+      q.string :location
+      q.integer :costume_inventory
+      q.integer :num_of_employees
+      q.boolean :still_in_business
+      q.datetime :opening_time
+      q.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |q|
+      q.string :name
+      q.string :location
+      q.string :theme
+      q.float :price
+      q.boolean :family_friendly
+      q.datetime :opening_date
+      q.datetime :closing_date
+      q.text :description
+    end
+  end
+end
