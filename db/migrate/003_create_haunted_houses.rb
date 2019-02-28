0612c95e6c45c7b8d138a1ed81d0477622072954
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-28 23:06:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,12 +5,12 @@ def change
   create_table :haunted_houses do |hauntedhouse|
   hauntedhouse.string  :name
   hauntedhouse.string  :location
+  hauntedhouse.float :price
   hauntedhouse.string  :theme
-  hauntedhouse.integer :price
-  hauntedhouse.boolean :whether_theyre_family_friendly_or_not
+  hauntedhouse.boolean :family_friendly
   hauntedhouse.datetime :opening_date
   hauntedhouse.datetime :closing_date
-  hauntedhouse.text :long_description
+  hauntedhouse.text :description
 
   hauntedhouse.timestamps null: false
   end
