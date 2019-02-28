de0fa85b3078171384661fa3302bc7a40d33a658
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-28 22:59:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       stores.string :name
       stores.string :location
       stores.integer  :costume_inventory
-      stores.integer :number_of_employees
+      stores.integer :num_of_employees
       stores.boolean :whether_or_not_its_still_in_business
       stores.datetime :opening_time
       stores.datetime :closing_time
