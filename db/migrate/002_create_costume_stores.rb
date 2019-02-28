7a29bd874fdcebe23a07bc6b96bb4f8326624d08
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-28 17:59:32 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+# class CreateCostumeStores < ActiveRecord::Migration[4.2]
+#
+  def change
+    create_table :costume_stores do |stores|
+      stores.string :name
+      stores.string :location
+      stores.integer  :costume_inventory
+      stores.integer :number_of_employees
+      stores.boolean :whether_or_not_its_still_in_business
+      stores.datetime :opening_time
+      stores.datetime :closing_time
+     end
+   end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+# class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+#
+def change
+  create_table :haunted_houses do |hauntedhouse|
+  hauntedhouse.string  :name
+  hauntedhouse.string  :location
+  hauntedhouse.string  :theme
+  hauntedhouse.integer :price
+  hauntedhouse.boolean :whether_theyre_family_friendly_or_not
+  hauntedhouse.datetime :opening_date
+  hauntedhouse.datetime :closing_date
+  hauntedhouse.text :long_description
+
+  hauntedhouse.timestamps null: false
+  end
+ end
+end
