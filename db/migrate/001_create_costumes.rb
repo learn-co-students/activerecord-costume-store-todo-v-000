d244d32e65e3e3b191c0f2f9fc07ef2c0ffb4aaf
Chao Hsuan Chi <allen0413@gmail.com>
2019-03-19 02:35:56 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,13 +1,13 @@
 # Create a class and inherit from ActiveRecord::Migration
-class CreateCostumes < ActiveRecord::Migration[4.2]
+class CreateCostumes < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
-      t.text :size
+      t.float :price
+      t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps null: false
     end
   end
 
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,15 +1,16 @@
 # Create your costume_stores migration here
-class CreateCostumeStores < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
 
   def change
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees
+      t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamps null: false
     end
   end
 
