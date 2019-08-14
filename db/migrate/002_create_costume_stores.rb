149aebddd4bcd4249ef70741559daf96cb662219
Tim Knoll <timothy.knoll@gmail.com>
2019-08-14 01:57:44 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.0]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location
-      t.interger :costume_inventory
-      t.interger :num_of_employees
+      t.integer :costume_inventory
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
