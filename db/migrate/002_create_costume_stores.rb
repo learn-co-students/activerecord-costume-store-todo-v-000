9592c5be202aade6d59695e72dd63c37e1675d25
Christian V <viray.christianj@gmail.com>
2021-03-28 03:05:53 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migration[5.2]
+class CreateCostumeStores < ActiveRecord::Base[5.2]
   def change
   create_table :costume_stores do |t|
     t.string :name 
